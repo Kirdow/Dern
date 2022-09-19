1bb9d17ca8da98d6c768fb9925b02cbcb3c3091b
Dennis Rönn <10155603+Kirdow@users.noreply.github.com>
2022-09-19 18:40:17 +0200

    restore actual code

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,6 @@
 
 int main(int argc, char** argv)
 {
-	Dern::Registry reg;
-	std::cout << "Default State!\n";
-	std::cout << "Has Int[a]: " << reg.HasEntry<int>("a") << "\n";
-	std::cout << "Has String[a]: " << reg.HasEntry<std::string>("a") << "\n";
-	std::cout << "Get Int[a]: " << reg.GetEntry<int>("a") << "\n";
-	std::cout << "Get String[a]: \"" << reg.GetEntry<std::string>("a") << "\"\n";
-
-	reg.SetEntry<int>("a", 5);
-	std::cout << "\nSet Int[a]: 5\n";
-	std::cout << "Has Int[a]: " << reg.HasEntry<int>("a") << "\n";
-	std::cout << "Has String[a]: " << reg.HasEntry<std::string>("a") << "\n";
-	std::cout << "Get Int[a]: " << reg.GetEntry<int>("a") << "\n";
-	std::cout << "Get String[a]: \"" << reg.GetEntry<std::string>("a") << "\"\n";
-
-	reg.SetEntry<std::string>("a", "hello");
-	std::cout << "\nSet String[a]: \"hello\"\n";
-	std::cout << "Has Int[a]: " << reg.HasEntry<int>("a") << "\n";
-	std::cout << "Has String[a]: " << reg.HasEntry<std::string>("a") << "\n";
-	std::cout << "Get Int[a]: " << reg.GetEntry<int>("a") << "\n";
-	std::cout << "Get String[a]: \"" << reg.GetEntry<std::string>("a") << "\"\n";
-
-	reg.UnsetEntry("a");
-	std::cout << "\nUnset[a]\n";
-	std::cout << "Has Int[a]: " << reg.HasEntry<int>("a") << "\n";
-	std::cout << "Has String[a]: " << reg.HasEntry<std::string>("a") << "\n";
-	std::cout << "Get Int[a]: " << reg.GetEntry<int>("a") << "\n";
-	std::cout << "Get String[a]: \"" << reg.GetEntry<std::string>("a") << "\"\n";
-	
-
-	if (true) return 0;
 	if (argc <= 1)
 	{
 		std::cerr << "No file specified!" << std::endl;
